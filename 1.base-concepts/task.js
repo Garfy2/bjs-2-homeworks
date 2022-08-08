4cf6ab78cc42545a7c79d6fbc6c9e2d28d7872ef
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2022-08-09 00:01:24 +0300

    Решение по заданию 1
    Решение по заданию 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
+"use strict";
+
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+
+    let discr = Math.pow(b, 2) - 4 * a * c;
+
+    if (discr < 0) {
+        break;
+    } else if (discr === 0) {
+        arr.push(-b / (2 * a));
+    } else (discr > 0){
+        arr.push(((-b + Math.sqrt(d)) / (2 * a)), ((-b - Math.sqrt(d)) / (2 * a)));
+    };
+
   return arr; // array
 }
 
