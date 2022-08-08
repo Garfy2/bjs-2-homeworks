6ecaf57c1374967762639b7c2ae295d922b78064
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2022-08-09 00:36:11 +0300

    правки к заданию 1
    правки к заданию 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,18 +2,23 @@
 
 
 function solveEquation(a, b, c) {
-  let arr = [];
+  let arr;
   // код для задачи №1 писать здесь
 
-    let discr = Math.pow(b, 2) - 4 * a * c;
-
+    let discr = Math.pow(b, 2) - (4 * a * c);
+       
     if (discr < 0) {
-        break;
+        arr = [];
     } else if (discr === 0) {
-        arr.push(-b / (2 * a));
-    } else (discr > 0){
-        arr.push(((-b + Math.sqrt(d)) / (2 * a)), ((-b - Math.sqrt(d)) / (2 * a)));
-    };
+        let x = -b / (2 * a);
+        arr = [x];
+    } else if (discr > 0) {
+        let x1 = (-b + Math.sqrt(discr)) / (2 * a);
+        let x2 = (-b - Math.sqrt(discr)) / (2 * a);
+        arr = [x1, x2];
+    }
+       
+    
 
   return arr; // array
 }
